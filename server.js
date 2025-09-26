1e8c9dcb12fd0b41ec3df0076e1e1356d4dc9478
nic <nicolasb@gmail.com>
2025-09-26 14:37:21 +0200

    MInor error on server *

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/api/health', (req, res) => {
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
